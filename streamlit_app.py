90ddc3be15ce5acbf052c4ad25b0693e62fed1fb
GE_Teacher <104121258+GE-Teacher@users.noreply.github.com>
2023-02-06 03:19:43 +0000

    ác

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,36 +1,34 @@
 import pandas as pd
-
-# Create a DataFrame
-df = pd.DataFrame({'A': [1, 2, 3], 'B': [4, 5, 6], 'C': [7, 8, 9]})
-
-# Save the DataFrame as a CSV file
-df.to_csv('data.csv', index=False)
-
-
 import git
 
-# Clone the repository
-repo = git.Repo.clone_from('https://github.com/username/repo.git', 'repo')
 
-# Add the file to the repository
-repo.index.add(['data.csv'])
-
-# Commit the changes
-repo.index.commit('Add data.csv')
-
-# Push the changes to GitHub
-origin = repo.remote(name='origin')
-origin.push()
 
 
 import streamlit as st
 
 def save_csv():
     # Code to save the DataFrame as a CSV file
-    ...
+
+
+    # Create a DataFrame
+    df = pd.DataFrame({'A': [1, 2, 3], 'B': [4, 5, 6], 'C': [7, 8, 9]})
+    
+    # Save the DataFrame as a CSV file
+    df.to_csv('data.csv', index=False)
     
     # Code to push the CSV file to GitHub
-    ...
+    # Clone the repository
+    repo = git.Repo.clone_from('https://github.com/username/repo.git', 'repo')
+    
+    # Add the file to the repository
+    repo.index.add(['data.csv'])
+    
+    # Commit the changes
+    repo.index.commit('Add data.csv')
+    
+    # Push the changes to GitHub
+    origin = repo.remote(name='origin')
+    origin.push()
     
     # Show a success message
     st.success('Data saved to GitHub!')
