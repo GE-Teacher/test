c2605f1e9e9ada4b65c82fbb01b6174aec1aa605
GE_Teacher <104121258+GE-Teacher@users.noreply.github.com>
2023-02-06 03:31:49 +0000

    ạdhvg

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,38 @@
-import pandas as pd
-import git
-
-
-
-
 import streamlit as st
+import pandas as pd
 
-def save_csv():
-    # Code to save the DataFrame as a CSV file
+# Text files
+
+text_contents = '''
+Foo, Bar
+123, 456
+789, 000
+'''
+
+# Different ways to use the API
+
+st.download_button('Download CSV', text_contents, 'text/csv')
+st.download_button('Download CSV', text_contents)  # Defaults to 'text/plain'
+
+with open('myfile.csv', 't') as f:
+	st.download_button('Download CSV', f)  # Defaults to 'text/plain'
 
 
-    # Create a DataFrame
-    df = pd.DataFrame({'A': [1, 2, 3], 'B': [4, 5, 6], 'C': [7, 8, 9]})
-    
-    # Save the DataFrame as a CSV file
-    df.to_csv('data.csv', index=False)
-    
-    # Code to push the CSV file to GitHub
-    # Clone the repository
-    repo = git.Repo.clone_from('https://github.com/GE-Teacher/repo.git', 'repo')
-    
-    # Add the file to the repository
-    repo.index.add(['data.csv'])
-    
-    # Commit the changes
-    repo.index.commit('Add data.csv')
-    
-    # Push the changes to GitHub
-    origin = repo.remote(name='origin')
-    origin.push()
-    
-    # Show a success message
-    st.success('Data saved to GitHub!')
+# ---
+# Binary files
 
-# Add a button to trigger the save function
-if st.button('Save data'):
-    save_csv()
+binary_contents = b'whatever'
+
+# Different ways to use the API
+
+st.download_button('Download file', binary_contents)  # Defaults to 'application/octet-stream'
+
+with open('myfile.zip', 'b') as f:
+	st.download_button('Download Zip', f)  # Defaults to 'application/octet-stream'
+
+
+# You can also grab the return value of the button,
+# just like with any other button.
+
+if st.download_button(...):
+	st.write('Thanks for downloading!')
