3bd0f35eafdd70f8400190ec05b97e2960f85ae3
GE_Teacher <104121258+GE-Teacher@users.noreply.github.com>
2023-02-06 08:15:52 +0000

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,24 +1,19 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
 
 st.title("Data Analysis App")
+
+st.write("Upload a CSV file to analyze its data")
 
 uploaded_file = st.file_uploader("Choose a CSV file", type=["csv"])
 if uploaded_file is not None:
     data = pd.read_csv(uploaded_file)
-    st.dataframe(data.head())
-
+    st.write("Data preview:")
+    st.write(data.head())
+    
+    if st.checkbox("Show value counts"):
+        st.write(data.iloc[:, -1].value_counts().plot(kind='bar'))
+        st.pyplot()
+        
     if st.checkbox("Show summary statistics"):
         st.write(data.describe())
-
-    if st.checkbox("Plot histogram"):
-        st.write(data.hist(bins=10, figsize=(10, 5)))
-        st.pyplot()
-
-    if st.checkbox("Plot bar chart"):
-        categorical_column = st.selectbox("Select a categorical column", data.columns.tolist())
-        st.write(data.groupby(categorical_column).size().plot(kind='bar'))
-        st.pyplot()
-else:
-    st.write("Please upload a CSV file")
